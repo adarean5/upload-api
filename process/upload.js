86904a19fe391185be0a9e06ceb2e2ef8c09a824
Jernej Strazisar <strazisar.jernej@gmail.com>
2019-03-13 10:32:09 +0100

    Code cleanup and documentation.

--- process/upload.js
+++ process/upload.js
@@ -5,37 +5,39 @@ const originalPath = process.argv[3];
 
 const newPath = __dirname + '/../uploads/' + originalName;
 
-// Testing timeout
-setTimeout(() => {
-  // Check if file already exists
-  fs.access(newPath, fs.F_OK, err => {
-    if (err) {
-      console.error(err);
-      // Delete the old file, to be replaced with the new one
-      fs.unlink(newPath, err => {
-        if (err) {
-          console.error(err);
-        }
+// Check if file already exists
+fs.access(newPath, fs.F_OK, err => {
+  if (err) {
+    console.error(err);
+    // If the file already exists delete and replace it with a newer version.
+    fs.unlink(newPath, err => {
+      if (err) {
+        console.log('File did not previously exist.');
+      }
 
-        // Read the old file, save it as new file and delete the old file
-        fs.readFile(originalPath, (err, data) => {
-          fs.writeFile(newPath, data, err => {
-            if (err) {
-              console.log(err);
-            } else {
-              fs.unlink(originalPath, err => {
-                if (err) {
-                  throw err;
-                } else {
-                  console.log(
-                    'File: "' + originalName + '" uploaded successfully.'
-                  );
-                }
-              });
-            }
-          });
+      /* 
+      Read the file from /tmp/, 
+        copy it to /uploads/ and rename it to it's original name,
+        delete the file in /tmp/.
+      */
+
+      fs.readFile(originalPath, (err, data) => {
+        fs.writeFile(newPath, data, err => {
+          if (err) {
+            console.log(err);
+          } else {
+            fs.unlink(originalPath, err => {
+              if (err) {
+                throw err;
+              } else {
+                console.log(
+                  'File: "' + originalName + '" uploaded successfully.'
+                );
+              }
+            });
+          }
         });
       });
-    }
-  });
-}, 15000);
+    });
+  }
+});
--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-const File = require('./models/File');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const child_process = require('child_process');
 const fs = require('fs');
-var url = require('url');
+/** @param file - Model that represents a file. */
+const File = require('./models/file');
+/**
+ * @param multer - Multer is a node.js middleware for handling
+ * multipart/form-data, used for uploading files.
+ */
+const multer = require('multer');
 
-//Initiate our app
+/** @param app - Express instance. */
 const app = express();
 
+/** @param TIMEOUT - Timeout used for testing the API. */
+const TIMEOUT = 5000;
+/** @param UPLOADS_FOLDER - Path to folder that contains uploaded files. */
 const UPLOADS_FOLDER = './uploads/';
+
+/**
+ * Simple auth params.
+ *
+ * @param CHECK_AUTH_FOR_EVERY_REQUEST
+ * @param AUTH_TOKEN
+ * @param AUTH_USERNAME
+ * @param AUTH_PASSWORD
+ */
 const CHECK_AUTH_FOR_EVERY_REQUEST = false;
 const AUTH_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 const AUTH_USERNAME = 'test';
 const AUTH_PASSWORD = 'test';
 
+/** @param upload - Multer middleware for the temporary upload folder. */
 const upload = multer({ dest: 'tmp/' });
-const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
-//CORS middleware
+/** @var allowCrossDomain - CORS middleware. */
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -28,6 +44,7 @@ var allowCrossDomain = function(req, res, next) {
   next();
 };
 
+/** @var checkAuthentication - Simple auth middleware. */
 var checkAuthentication = function(req, res, next) {
   if (!CHECK_AUTH_FOR_EVERY_REQUEST) {
     next();
@@ -46,62 +63,60 @@ var checkAuthentication = function(req, res, next) {
   res.sendStatus(401).end();
 };
 
-//Configure our app
+// Server middleware configuration.
 app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(checkAuthentication);
 
-// Commented code was used strictly for testing purposes and serves no purpose in the API
+// Simple login.
+app.post('/login', function(req, res) {
+  const user = req.body;
 
-// Load the homepage
-/*app.get('/', function(req, res) {
-  console.log('Got a GET request for index.html.');
-  res.sendFile(__dirname + '/public/index.html');
-});*/
-
-// Load the upload file page
-/*app.get('/upload-page', function(req, res) {
-  console.log('Got a GET request for file upload.');
-  res.sendFile(__dirname + '/public/upload.html');
-});*/
-
-// Post form
-/*app.post('/process-post', urlEncodedParser, function(req, res) {
-  response = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-});*/
+  if (user.userName === AUTH_USERNAME && user.password === AUTH_PASSWORD) {
+    res.json({
+      userName: user.userName,
+      authToken: AUTH_TOKEN
+    });
+  } else {
+    res.sendStatus(401).end();
+  }
+});
 
 // File upload
 app.post('/file-upload', upload.single('file-to-upload'), (req, res) => {
   console.log('Got a POST request for file upload.');
   if (req.file) {
+    /** @member originalPath - Path to the file in the /tmp/ folder. */
     let originalPath = req.file.path;
+    /**
+     * @member originalName - Name of the file before it was saved to /tmp/.
+     * (Once the file is saved to the /tmp/ folder it gets a new name.)  */
     let originalName = req.file.originalname;
 
-    const uploadProcess = child_process.spawn('node', [
-      './process/upload.js',
-      originalName,
-      originalPath
-    ]);
+    // Testing timeout
+    setTimeout(() => {
+      // Start a new upload process.
+      const uploadProcess = child_process.spawn('node', [
+        './process/upload.js',
+        originalName,
+        originalPath
+      ]);
 
-    uploadProcess.stdout.on('data', function(data) {
-      console.log('Stdout: ' + data);
-    });
+      uploadProcess.stdout.on('data', function(data) {
+        console.log('Stdout: ' + data);
+      });
 
-    uploadProcess.stderr.on('data', function(data) {
-      console.log('Stderr: ' + data);
-    });
+      uploadProcess.stderr.on('data', function(data) {
+        console.log('Stderr: ' + data);
+      });
 
-    uploadProcess.on('close', function(code) {
-      console.log('Upload process exited with code ' + code);
-      res.end();
-    });
+      uploadProcess.on('close', function(code) {
+        console.log('Upload process exited with code ' + code);
+        res.end();
+      });
+    }, TIMEOUT);
   } else {
     res.end();
   }
@@ -109,31 +124,39 @@ app.post('/file-upload', upload.single('file-to-upload'), (req, res) => {
 
 // File download
 app.post('/download', function(req, res) {
+  /** @member fileName - Name of the file that was requested for download. */
   const fileName = req.body.fileName;
   console.log('POST request for ' + fileName);
+
   // Testing timeout
   setTimeout(() => {
     res.sendFile(__dirname + '/uploads/' + fileName, err => {
       console.log('FIle ' + fileName + ' sent');
     });
-  }, 150);
+  }, TIMEOUT);
 });
 
 // Delete file
 app.delete('/delete/:fileName', function(req, res) {
+  /** @member fileName - Name of the file that was requested for deletion. */
   const fileName = [req.params.fileName].toString();
 
   console.log('DELETE request for file ' + fileName);
 
-  fs.unlink(__dirname + '/uploads/' + fileName, err => {
-    res.end();
-  });
+  setTimeout(() => {
+    fs.unlink(__dirname + '/uploads/' + fileName, err => {
+      res.end();
+    });
+  }, TIMEOUT);
 });
 
 // List all files
 app.get('/list-files', function(req, res) {
   console.log(new Date(), ' GET request to list-files');
+  // Array with file info to be sent as a response.
   let fileArray = [];
+  // How many files are left to process in to the fileArray,
+  //  so we know when to end the response in the callback.
   let filesToProcess = -1;
   fs.readdir(UPLOADS_FOLDER, (err, files) => {
     filesToProcess = files.length;
@@ -152,20 +175,7 @@ app.get('/list-files', function(req, res) {
   });
 });
 
-app.post('/login', function(req, res) {
-  const user = req.body;
-
-  if (user.userName === AUTH_USERNAME && user.password === AUTH_PASSWORD) {
-    res.json({
-      userName: user.userName,
-      authToken: AUTH_TOKEN
-    });
-  } else {
-    res.sendStatus(401).end();
-  }
-});
-
-// Start the server
+/** @var server - Start the server. */
 var server = app.listen(8081, function() {
   var host = server.address().address;
   var port = server.address().port;
